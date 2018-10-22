5febe989750dee4fbf79cac835bf439a3e6748cb
MidhunBalan <midhun.balan@a-cti.com>
2018-10-22 19:09:49 +0530

    Updated code and add a service layer for the API.

--- messenger/src/main/java/org/midhun/jaxrs/messenger/resources/ApiEndPoint.java
+++ messenger/src/main/java/org/midhun/jaxrs/messenger/resources/ApiEndPoint.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("messages")
-public class MessageResources {
+public class ApiEndPoint {
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
---
+++ messenger/src/main/java/org/service/jaxrs/messenger/ApiServiceLayer.java
@@ -0,0 +1,6 @@
+package org.service.jaxrs.messenger;
+
+public class ApiServiceLayer {
+	
+
+}
